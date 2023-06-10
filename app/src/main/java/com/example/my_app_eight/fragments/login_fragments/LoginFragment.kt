31b6a1d6cd8bd10457e393c229594b3cbaa3c0c8
Vlad <tsoiivv@gmail.com>
2023-06-11 00:12:31 +0600

    added login fragment with ui and its implementation

--- app/src/main/java/com/example/my_app_eight/fragments/login_fragments/LoginFragment.kt
+++ app/src/main/java/com/example/my_app_eight/fragments/login_fragments/LoginFragment.kt
@@ -1,6 +1,8 @@
 package com.example.my_app_eight.fragments.login_fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -22,5 +24,34 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        checkInput()
+    }
+
+    private fun checkInput() {
+        binding.inputLogin.addTextChangedListener(inputTextWatcher)
+        binding.inputLoginPassword.addTextChangedListener(inputTextWatcher)
+    }
+
+    private val inputTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val usernameInput = binding.inputLogin.text.toString().trim()
+            val passwordInput = binding.inputLoginPassword.text.toString().trim()
+
+            val button = binding.btnLogin
+
+            if (passwordInput.isEmpty() || usernameInput.isEmpty()) {
+                button.isEnabled = false
+                button.setBackgroundResource(R.drawable.grey_btn)
+            } else {
+                button.isEnabled = true
+                button.setBackgroundResource(R.drawable.rounded_btn)
+            }
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+        }
     }
 }
