5cae4adf3b7581b22444ade80a93268f45742032
Vlad <tsoiivv@gmail.com>
2023-06-10 22:58:32 +0600

    created project/added splash screen and all dependecies for project

---
+++ app/src/main/java/com/example/my_app_eight/fragments/login_fragments/LoginFragment.kt
@@ -0,0 +1,26 @@
+package com.example.my_app_eight.fragments.login_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.my_app_eight.R
+import com.example.my_app_eight.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =  FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
