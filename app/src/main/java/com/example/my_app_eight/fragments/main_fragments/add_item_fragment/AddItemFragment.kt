2cb862f5b93976e475f66f1dc6ebc8c5ba21b89a
Vlad <tsoiivv@gmail.com>
2023-06-19 23:01:38 +0600

    added images loading for adding page

--- app/src/main/java/com/example/my_app_eight/fragments/main_fragments/add_item_fragment/AddItemFragment.kt
+++ app/src/main/java/com/example/my_app_eight/fragments/main_fragments/add_item_fragment/AddItemFragment.kt
@@ -1,10 +1,18 @@
 package com.example.my_app_eight.fragments.main_fragments.add_item_fragment
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.my_app_eight.R
 import com.example.my_app_eight.databinding.FragmentAddItemBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -12,6 +20,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class AddItemFragment : Fragment() {
 
     private lateinit var binding : FragmentAddItemBinding
+    private val PICK_IMAGE_REQUEST = 1
+
+    private lateinit var addButton: ImageView
+    private lateinit var imageContainer: ViewGroup
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,8 +33,42 @@ class AddItemFragment : Fragment() {
         binding = FragmentAddItemBinding.inflate(inflater, container, false)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        callGallery()
+    }
+
+    private fun callGallery() {
+
+        addButton = binding.addImg
+        imageContainer = binding.imageContainer
+
+        addButton.setOnClickListener {
+            openGallery()
+        }
+    }
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            val imageUri = data.data
+            val imageView = ImageView(requireContext())
+            val layoutParams = LinearLayout.LayoutParams(
+                addButton.width,
+                addButton.height
+            )
+            layoutParams.setMargins(6, 0, 6, 0) // Set margin of 6dp
+            imageView.layoutParams = layoutParams
+            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+            imageView.setImageURI(imageUri)
+            imageView.setBackgroundResource(R.drawable.rounded_image_background)
+            imageView.clipToOutline = true // Add rounded corners
+            imageContainer.addView(imageView)
+        }
     }
 }
