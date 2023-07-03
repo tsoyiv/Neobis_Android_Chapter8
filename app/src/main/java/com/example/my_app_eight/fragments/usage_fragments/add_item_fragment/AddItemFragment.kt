490fcf3d3a50bbccffbcf347a9cbe65393a6bf81
Vlad <tsoiivv@gmail.com>
2023-07-03 13:08:08 +0600

    check commit

--- app/src/main/java/com/example/my_app_eight/fragments/usage_fragments/add_item_fragment/AddItemFragment.kt
+++ app/src/main/java/com/example/my_app_eight/fragments/usage_fragments/add_item_fragment/AddItemFragment.kt
@@ -22,27 +22,20 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.my_app_eight.HomeActivity
 import com.example.my_app_eight.R
-import com.example.my_app_eight.api.RetrofitInstance
 import com.example.my_app_eight.databinding.FragmentAddItemBinding
-import com.example.my_app_eight.models.ProductPostRequest
-import com.example.my_app_eight.models.ProductResponse
-import com.example.my_app_eight.util.Holder
-import com.example.my_app_eight.util.Holder.selectedImageUri
 import com.example.my_app_eight.view_models.item_view_model.AddItemViewModel
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.custom_dialog_logout.view.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class AddItemFragment : Fragment() {
 
-    private lateinit var binding : FragmentAddItemBinding
+    private lateinit var binding: FragmentAddItemBinding
     private val PICK_IMAGE_REQUEST = 1
     private lateinit var addButton: ImageView
     private lateinit var imageContainer: ViewGroup
     private var selectedImageFile: Uri? = null
     private val viewModel: AddItemViewModel by viewModels()
+    private val imageList: MutableList<String> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -51,31 +44,88 @@ class AddItemFragment : Fragment() {
         binding = FragmentAddItemBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (requireActivity() as HomeActivity).hide()
 
         callGallery()
         cancelBtn()
-        saveItem()
+        //saveItem()
     }
 
-    private fun inputItem() {
-        val name = binding.editTextName.text.toString()
-        val price = binding.editTextPrice.text.toString()
-        val description = binding.editTextShortD.text.toString()
-        val photo = selectedImageFile
+//    private fun inputItem() {
+//        val name = binding.editTextName.text.toString()
+//        val price = binding.editTextPrice.text.toString()
+//        val description = binding.editTextShortD.text.toString()
+//        val photo = imageList.firstOrNull()
+//
+//        viewModel.inputItem(name, price, description, imageList)
+//
+//        viewModel.itemAddedSuccess.observe(viewLifecycleOwner) { isSuccess ->
+//            if (isSuccess) {
+//                Toast.makeText(requireContext(), "Product added", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(requireContext(), "Failed to add item", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
 
-        viewModel.inputItem(name, price, description, photo)
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
 
-        viewModel.itemAddedSuccess.observe(viewLifecycleOwner) { isSuccess ->
-            if (isSuccess) {
-                Toast.makeText(requireContext(), "Product added", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(requireContext(), "Failed to add item", Toast.LENGTH_SHORT).show()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            val imageUri = data.data
+            val imageView = ImageView(requireContext())
+            val layoutParams = LinearLayout.LayoutParams(
+                addButton.width,
+                addButton.height
+            )
+            layoutParams.setMargins(6, 0, 6, 0) // Set margin of 6dp
+            imageView.layoutParams = layoutParams
+            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+            imageView.setImageURI(imageUri)
+            imageView.clipToOutline = true
+            imageContainer.addView(imageView)
+
+            imageUri?.let {
+                val imagePath = getImagePath(imageUri)
+                imagePath?.let {
+                    imageList.clear()  // Clear the existing image list
+                    imageList.add(it)  // Add the selected image path
+                }
             }
         }
     }
+
+
+    private fun getImagePath(uri: Uri): String? {
+        val implementOfImg = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = requireActivity().contentResolver.query(uri, implementOfImg, null, null, null)
+        val columnIndex = cursor?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        cursor?.moveToFirst()
+
+        val imagePath = columnIndex?.let { cursor.getString(it) }
+        cursor?.close()
+
+        return imagePath
+    }
+
+//    private fun uriToFile(uri: Uri): File {
+//        val filePath = requireActivity().contentResolver.openInputStream(uri)?.use { inputStream ->
+//            val file = File(requireContext().cacheDir, "temp_image")
+//            FileOutputStream(file).use { outputStream ->
+//                inputStream.copyTo(outputStream)
+//            }
+//            file.absolutePath
+//        }
+//        return File(filePath)
+//    }
 
     private fun callDialog() {
         val dialogBinding = layoutInflater.inflate(R.layout.custom_cancel_editing, null)
@@ -96,12 +146,12 @@ class AddItemFragment : Fragment() {
         }
     }
 
-    private fun saveItem() {
-        binding.btnReadyAddItem.setOnClickListener {
-            callSnackBarAndNavigate()
-            inputItem()
-        }
-    }
+//    private fun saveItem() {
+//        binding.btnReadyAddItem.setOnClickListener {
+//            callSnackBarAndNavigate()
+//            inputItem()
+//        }
+//    }
 
     private fun cancelBtn() {
         binding.btnCancelAddItem.setOnClickListener {
@@ -146,31 +196,31 @@ class AddItemFragment : Fragment() {
         }, 500)
     }
 
-    private fun openGallery() {
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        intent.type = "image/*"
-        startActivityForResult(intent, PICK_IMAGE_REQUEST)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
-            val imageUri = data.data
-            val imageView = ImageView(requireContext())
-            val layoutParams = LinearLayout.LayoutParams(
-                addButton.width,
-                addButton.height
-            )
-            layoutParams.setMargins(6, 0, 6, 0) // Set margin of 6dp
-            imageView.layoutParams = layoutParams
-            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-            imageView.setImageURI(imageUri)
-            imageView.setBackgroundResource(R.drawable.rounded_image_background)
-            imageView.clipToOutline = true // Add rounded corners
-            imageContainer.addView(imageView)
-
-            // Store the image URI in the selectedImageUri variable
-            selectedImageUri = imageUri
-        }
-    }
+//    private fun openGallery() {
+//        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+//        intent.type = "image/*"
+//        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+//            val imageUri = data.data
+//            val imageView = ImageView(requireContext())
+//            val layoutParams = LinearLayout.LayoutParams(
+//                addButton.width,
+//                addButton.height
+//            )
+//            layoutParams.setMargins(6, 0, 6, 0) // Set margin of 6dp
+//            imageView.layoutParams = layoutParams
+//            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+//            imageView.setImageURI(imageUri)
+//            imageView.setBackgroundResource(R.drawable.rounded_image_background)
+//            imageView.clipToOutline = true // Add rounded corners
+//            imageContainer.addView(imageView)
+//
+//            // Store the image URI in the selectedImageUri variable
+//            selectedImageUri = imageUri
+//        }
+//    }
 }
